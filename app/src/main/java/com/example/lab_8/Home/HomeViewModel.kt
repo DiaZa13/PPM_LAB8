a845fc12869194f4346bcb73b933875d19ae7e6e
Diana Corado <cor191025@uvg.edu.gt>
2020-05-31 12:42:18 -0600

    LAB_8

---
+++ app/src/main/java/com/example/lab_8/Home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.lab_8.Home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/lab_8/News/NewsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.lab_8.News
+
+import androidx.lifecycle.ViewModel
+
+class NewsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
